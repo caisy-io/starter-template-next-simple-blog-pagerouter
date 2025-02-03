0687971f4071bcd089881cdbab92dcc1b8acde86
Moritz Gruber <m.gruber@cyclus.digital>
2025-02-03 16:54:54 +0100

    chore: update example with live preview usage
    chore: update example with live preview usage

--- src/components/Headline.tsx
+++ src/components/Headline.tsx
@@ -1,18 +1,29 @@
 import React from "react";
 import { IGenHeadline } from "../services/graphql/__generated/sdk";
 import { CenterContainer } from "./CenterContainer";
+import { getCaisyInspectProps } from "@caisy/live-preview-react";
 
-export const Headline: React.FC<IGenHeadline> = ({ headline, subheadline }) => {
+export const Headline: React.FC<IGenHeadline> = ({
+  headline,
+  subheadline,
+  id,
+}) => {
   return (
     <CenterContainer>
       <div className="mb-8 flex flex-col justify-start items-center gap-2.5">
         {headline && (
-          <h1 className="text-4xl font-bold text-left text-slate-900">
+          <h1
+            {...getCaisyInspectProps({ id: id!, fieldName: "headline" })}
+            className="text-4xl font-bold text-left text-slate-900"
+          >
             {headline}
           </h1>
         )}
         {subheadline && (
-          <h4 className="mt-2 text-xl text-center text-gray-400">
+          <h4
+            {...getCaisyInspectProps({ id: id!, fieldName: "subheadline" })}
+            className="mt-2 text-xl text-center text-gray-400"
+          >
             {subheadline}
           </h4>
         )}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,55 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import { Toast } from "../components/Toast";
 import { Footer } from "../layouts/Footer";
 import { Navigation } from "../layouts/Navigation";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import "@caisy/live-preview-react/index.css";
+import {
+  CaisyConnectionIndicator,
+  caisyLivePreview,
+  useCaisyUpdates,
+} from "@caisy/live-preview-react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const livePreviewEnabled =
+    `${process.env.NEXT_PUBLIC_USE_DRAFT_MODE}` === "true";
+
+  useEffect(() => {
+    if (typeof window === "undefined") return;
+
+    const token = router.query.caisy_preview_access_token as string;
+    const projectId = router.query.project_id as string;
+
+    if (!token || !projectId) {
+      return;
+    }
+
+    const close = caisyLivePreview({
+      projectId,
+      token,
+      locale: router.locale,
+      enabled: livePreviewEnabled,
+    });
+
+    return () => {
+      close && close();
+    };
+  }, [router.locale, router.query, livePreviewEnabled]);
+
+  const livePageProps = useCaisyUpdates(pageProps);
+
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width" />
       </Head>
-      {process.env.NEXT_PUBLIC_SHOW_ONBOARDING_TOAST != "false" && <Toast />}
-      {pageProps.Navigation && <Navigation {...pageProps.Navigation} />}
-      <Component {...pageProps} />
-      {pageProps.Footer && <Footer {...pageProps.Footer} />}
+      {livePreviewEnabled && <CaisyConnectionIndicator />}
+      {livePageProps.Navigation && <Navigation {...livePageProps.Navigation} />}
+      <Component {...livePageProps} />
+      {livePageProps.Footer && <Footer {...livePageProps.Footer} />}
     </>
   );
 }
